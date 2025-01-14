fc09bd6c733b27dc7abfd3c751ead6f2ee93ac27
alexsalas <alexsalas@ceroca.cat>
2025-01-14 16:33:55 +0100

    Slidebar
    Checkbox funcionando y esborrar tasca

--- lib/componentes/item_tasques.dart
+++ lib/componentes/item_tasques.dart
@@ -1,47 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ItemTasques extends StatelessWidget {
   final String textTasca;
-  final bool  valorCheckbox;
-  const ItemTasques({
-    super.key, 
-    required this.textTasca, 
-    required this.valorCheckbox
-  });
+  final bool valorCheckbox;
+  final Function(bool?)? canviaValorCheckbox;
+  final Function(BuildContext)? esborraTasca;
+  const ItemTasques(
+      {super.key,
+      required this.textTasca,
+      required this.valorCheckbox,
+      required this.esborraTasca,
+      required this.canviaValorCheckbox});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.teal,
-          borderRadius: BorderRadius.circular(10),
+      //* Slidable = se puede arrastrar
+      child: Slidable(
+        endActionPane: ActionPane(
+          //* Tipo de movimiento
+          motion: const StretchMotion(),
+
+          children: [
+            SlidableAction(
+              icon: Icons.delete,
+              backgroundColor: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              onPressed: esborraTasca,
+            )
+          ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
-          child: Row(
-            children: [
-              // Checkbox
-              Checkbox(
-                activeColor: Colors.teal[200],
-                checkColor: Colors.orange[200],
-                side: BorderSide(color: Colors.orange[200]!, width: 2),
-                value: true,
-                onChanged: (valorRebut) {},
-              ),
-                
-              // Text
-              Text(
-                textTasca,
-                style: TextStyle(
-                  color: Colors.orange[200],
-                  decoration:  valorCheckbox 
-                  ? TextDecoration.lineThrough 
-                  : TextDecoration.none, 
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+            child: Row(
+              children: [
+                // Checkbox
+                Checkbox(
+                  activeColor: Colors.teal[200],
+                  checkColor: Colors.orange[200],
+                  side: BorderSide(color: Colors.orange[200]!, width: 2),
+                  value: true,
+                  onChanged: canviaValorCheckbox,
                 ),
-              ),
-            ],
+
+                // Text
+                Text(
+                  textTasca,
+                  style: TextStyle(
+                    color: Colors.orange[200],
+                    decoration: valorCheckbox
+                        //* Si es true se pone la linea
+                        ? TextDecoration.lineThrough
+                        //* y si no no pongas nada
+                        : TextDecoration.none,
+                    decorationColor: Colors.orange[200],
+                    decorationThickness: 2,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pagines/pagina1.dart
+++ lib/pagines/pagina1.dart
@@ -9,13 +9,30 @@ class Pagina1 extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Pagina1> {
-
+  //* Listas
   List tasquesLlista = [
     {"titol": "Tasca 1", "valor": false},
     {"titol": "Tasca 2", "valor": true},
+    {"titol": "Tasca 3", "valor": true},
   ];
+
+  void canviaCheckbox(int posLlista) {
+    //* Para actualizar la pantalla 
+    setState(() {
+      //* ! =  es no true 
+      tasquesLlista[posLlista]["valor"] = !tasquesLlista[posLlista]["valor"];
+    });
+  }
+
+  void accioEsborraTasca(int posLlista) {
+    setState(() {
+      tasquesLlista.removeAt(posLlista);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    //* Scaffold
     return Scaffold(
       backgroundColor: Colors.teal[100],
       //* AppBar
@@ -26,29 +43,33 @@ class _MyWidgetState extends State<Pagina1> {
           style: TextStyle(color: Colors.orange), // Cambié esto aquí
         ),
       ),
+      //* FloatingActionButton = BOTON ABAJO DE LA DERECHA
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.teal[300],
         shape: const CircleBorder(),
-        onPressed: () {},
+        onPressed: () {
+          // Agregar nueva tarea (podrías agregar lógica aquí)
+        },
         child: const Icon(
           Icons.add,
           color: Colors.orange, // Cambié el color aquí también
         ),
       ),
 
-      //* Body,
+      //* Body
       body: ListView.builder(
+        //* Cuantos items habrán
         itemCount: tasquesLlista.length,
+        //* Recibe una funcion anónima
+        //* El index recorre la lista, es como la i de un for
         itemBuilder: (context, index) {
-
-          return   ItemTasques(
+          return ItemTasques(
             textTasca: tasquesLlista[index]["titol"],
             valorCheckbox: tasquesLlista[index]["valor"],
-            
-            
+            canviaValorCheckbox: (valor) => canviaCheckbox(index),
+            esborraTasca: (valor) => accioEsborraTasca(index),
           );
-
-        }
+        },
       ),
     );
   }
